49dcbe071c7771178c4850b5f188f672d78c1225
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-04-04 09:16:29 -0300

    Todo About and Menu components

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -2,10 +2,16 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
+import Todo from './todo/todo'
+import About from '../about/About'
+import Menu from '../template/Menu'
+
 
 
 export default props => (
     <div className="container">
-     <h1> Teste</h1>
+        <Menu/>
+        <Todo/>
+        <About/>
     </div>
 )
