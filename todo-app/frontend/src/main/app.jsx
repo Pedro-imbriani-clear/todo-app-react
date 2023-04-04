2297aad30526df06f876d520912a81af0a821a14
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-04-04 10:10:33 -0300

    adding routes, PageHeader and form

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -2,16 +2,15 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
-import Todo from './todo/todo'
-import About from '../about/About'
 import Menu from '../template/Menu'
+import Routes from './Routes'
 
 
 
 export default props => (
     <div className="container">
         <Menu/>
-        <Todo/>
-        <About/>
+        <Routes/>
+
     </div>
 )
